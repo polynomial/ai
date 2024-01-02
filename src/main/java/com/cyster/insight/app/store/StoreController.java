af4f8f58e9f03bb50b261d389bebcba528b00aab
Mark Cyster <mcyster@extole.com>
2024-01-01 17:59:36 -0800

    Errors on store load endpoint

--- src/main/java/com/cyster/insight/app/store/StoreController.java
+++ src/main/java/com/cyster/insight/app/store/StoreController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cyster.ai.vector.simple.SimpleVectorStoreService;
+import com.cyster.insight.app.RestException;
 
 @RestController
 public class StoreController {
 
     private SimpleVectorStoreService storeService;
-    
+
     public StoreController(SimpleVectorStoreService storeService) {
         this.storeService = storeService;
     }
@@ -27,17 +29,26 @@ public class StoreController {
     public Set<String> index() {
         return storeService.getStores();
     }
-    
+
     @GetMapping("/stores/{name}/query")
     public List<Document> query(
         @PathVariable("name") String name, @RequestParam String query) {
         return this.storeService.getStore(name).similaritySearch(SearchRequest.query(query));
     }
- 
+
     @PostMapping("/stores")
-    public Boolean load_store(@RequestBody StoreLoadRequest load) {
-        this.storeService.buildStore(load.getName(), load.getUriPrefix(), load.getPath());
+    public Boolean load_store(@RequestBody StoreLoadRequest load) throws RestException {
+        if (load.getName() == null) {
+            throw new RestException(HttpStatus.BAD_REQUEST, "name not specified");
+        }
+        if (load.getLoadPath() == null) {
+            throw new RestException(HttpStatus.BAD_REQUEST, "loadPath not specified");
+        }
+        if (load.getUriPrefix() == null) {
+            throw new RestException(HttpStatus.BAD_REQUEST, "uriPrefix not specified");
+        }
+        this.storeService.buildStore(load.getName(), load.getUriPrefix(), load.getLoadPath());
         return Boolean.TRUE;
     }
-    
+
 }
--- src/main/java/com/cyster/insight/app/store/StoreLoadRequest.java
+++ src/main/java/com/cyster/insight/app/store/StoreLoadRequest.java
@@ -1,26 +1,30 @@
 package com.cyster.insight.app.store;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class StoreLoadRequest {
     private String name;
     private String uriPrefix;
     private String loadPath;
-    
+
     public StoreLoadRequest(String name, String uriPrefix, String loadPath) {
         this.name = name;
         this.uriPrefix = uriPrefix;
         this.loadPath = loadPath;
     }
-    
+
+    @JsonProperty(required = true)
     public String getName() {
         return this.name;
     }
-    
+
+    @JsonProperty(required = true)
     public String getUriPrefix() {
         return this.uriPrefix;
     }
-    
-    public String getPath() {
+
+    @JsonProperty(required = true)
+    public String getLoadPath() {
         return this.loadPath;
     }
 }
-
