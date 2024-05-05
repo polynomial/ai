1f050108da20f755942f58d111390e15f7c35a43
Mark Cyster <mcyster@extole.com>
2024-05-05 14:37:01 -0700

    Listen for mentions of Beep Boop in ticket comments

--- jira-app/src/main/java/com/extole/app/jira/ticket/TicketController.java
+++ jira-app/src/main/java/com/extole/app/jira/ticket/TicketController.java
@@ -1,5 +1,9 @@
 package com.extole.app.jira.ticket;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.compress.archivers.zip.UnicodeCommentExtraField;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
@@ -25,6 +29,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 public class TicketController {
+    private static final String JIRA_APP_ACCOUNT_ID = "712020:ac07cb57-f120-4f67-a1d7-06e69eaae834";  // TODO: property
+    private static Pattern MENTION_PATTERN = Pattern.compile("\\[\\~accountid:[^\\]]+\\]");
+    
     private TicketCommenter ticketCommenter;
 
     private static final Logger logger = LogManager.getLogger(TicketController.class);
@@ -38,31 +45,58 @@ public class TicketController {
     public ResponseEntity<Void> ticketEvent(@RequestBody JsonNode request) throws BadRequestException, FatalException {
         eventLogger.info(request.toString());
 
-        if (!request.has("issue_event_type_name") || !request.path("issue_event_type_name").asText().equals(
-            "issue_created")) {
-            throw new BadRequestException("Unexpected event: " + request.toString());
+        if (!request.has("issue")) {
+            throw new BadRequestException("Unexpected jira event, no issue attribute: " + request.toString());
         }
-
+        if (!request.path("issue").has("key")) {
+            throw new BadRequestException("Unexpected jira event, issue attribute has no key: " + request.toString());
+        }        
         var ticketNumber = request.path("issue").path("key").asText();
-        logger.info("Ticket - checking: " + ticketNumber);
-
-        if (!ticketNumber.toLowerCase().startsWith("sup")) {
-            logger.info("Ticket - " + ticketNumber + " ignored: only processing SUP tickets");
-            return ResponseEntity.ok().build();
-        }
-
-        if (!request.has("webhookEvent")) {
-            logger.info("Ticket - " + ticketNumber + " ignored: no webookEvent attribute");
-            return ResponseEntity.ok().build();
-        }
-            
-        if (!request.path("webhookEvent").asText().equalsIgnoreCase("jira:issue_created")) {
-             logger.info("Ticket - " + ticketNumber + " ignored: only processing jira:issue_created events");
-             return ResponseEntity.ok().build();
-        }
         
-        ticketCommenter.process(ticketNumber);
+        if (!request.has("webhookEvent")) {
+            throw new BadRequestException("Unexpected jira event, no webhookEvent attribute: " + request.toString());
+        }
+        var webhookEvent = request.path("webhookEvent").asText().toLowerCase();
+        
+        logger.info("Ticket - checking: " + ticketNumber);
+ 
+        switch (webhookEvent) {
+        case "jira:issue_created":
+            if (ticketNumber.toLowerCase().startsWith("sup")) {
+                logger.info("Ticket - " + ticketNumber + " - issue_created - processing");                                
+                ticketCommenter.process(ticketNumber);
+            } else {
+                logger.info("Ticket - " + ticketNumber + " issue_created - ignored - only processing SUP tickets");
+            }
+            break;
+            
+        case "comment_created":
+            if (!request.has("comment")) {
+                logger.info("Ticket - " + ticketNumber + " - comment_created - has no comment - ignoring");                
+            }
+            else if (!request.get("comment").has("body")) {
+                logger.info("Ticket - " + ticketNumber + " - comment_created - comment has no body - ignoring");                                
+            }
+            var comment = request.get("comment").get("body").asText();
+            Matcher matcher = MENTION_PATTERN.matcher(comment);
 
+            boolean mention = false;
+            while (matcher.find()) {
+                if (matcher.group().equals("[~accountid:" + JIRA_APP_ACCOUNT_ID + "]")) {
+                    mention = true;
+                    break;
+                }
+            }
+            if (mention) {
+               logger.info("Ticket - " + ticketNumber + " - comment_created - ai mention");                                
+               ticketCommenter.process(ticketNumber);  // TODO pass comment as prompt
+            }
+            break;
+
+        default:
+            logger.info("Ticket - " + ticketNumber + " - " + webhookEvent + " ignored");
+       }
+       
         return ResponseEntity.ok().build();
     }
 
@@ -83,4 +117,5 @@ public class TicketController {
             super(message, cause);
         }
     }
+    
 }
