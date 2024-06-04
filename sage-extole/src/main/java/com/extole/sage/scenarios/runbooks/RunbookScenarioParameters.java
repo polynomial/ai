bd521e67f1f8828ff74ac1cc8a42c80fe2dd2530
Mark Cyster <mcyster@extole.com>
2024-06-03 18:12:01 -0700

    Fix json annotation on RunbookScenarioParameters

--- sage-extole/src/main/java/com/extole/sage/scenarios/runbooks/RunbookScenarioParameters.java
+++ sage-extole/src/main/java/com/extole/sage/scenarios/runbooks/RunbookScenarioParameters.java
@@ -1,11 +1,13 @@
 package com.extole.sage.scenarios.runbooks;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class RunbookScenarioParameters {
     private String ticketNumber;
     
-    public RunbookScenarioParameters(String ticketNumber) {
+    @JsonCreator
+    public RunbookScenarioParameters(@JsonProperty("ticketNumber") String ticketNumber) {
         this.ticketNumber = ticketNumber;
     }
     
